15682132d41bbaaa12caa3dd15f3406acd2da90a
Shohanur Rahman <shohanur.rahman57@gmail.com>
2026-02-05 22:12:54 +0600

    39-html-template-setup

---
+++ JobFinderApplication/JobFinderWebApp/Models/DbTodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace JobFinderWebApp.Models
+{
+    public class DbTodoItem
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Address { get; set; }
+    }
+}
---
+++ JobFinderApplication/JobFinderWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JobFinderWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
